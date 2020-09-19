e86b1a5183b6dee3deec92d2a478bb6fa6a7fa89
TylerPelaez <tyler.pelaez.11@gmail.com>
2020-09-19 00:38:18 -0400

    Implemented animations

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,51 +8,57 @@ export var FRICTION := 500
 export var AIM_SPEED := 1
 export var CAMERA_POSITION_OFFSET_DENOMINATOR := 4
 
-# state - currently just move
-enum {
-	MOVE
-}
-
 var velocity = Vector2.ZERO
-var state = MOVE
-var playerGun
+var playerGun = null
 
-onready var animationPlayer = $AnimationPlayer
+onready var headAnimationPlayer = $HeadAnimationPlayer
+onready var torsoAnimationPlayer = $TorsoAnimationPlayer
+onready var feetAnimationPlayer = $FeetAnimationPlayer
 onready var cameraFollow = $CameraFollow
+onready var feetSprite = $FeetSprite
+onready var feetSpriteDefaultRotation = feetSprite.rotation
 
 # TODO: Don't instantiate the gun on init
 func _ready():
-	playerGun = PlayerGun.instance()
-	add_child(playerGun)
-	playerGun.global_position = global_position
-	playerGun.rotation = rotation
+	spawnPlayerGun()
 
 func _physics_process(delta):
-	match state:
-		MOVE:
-			move_state(delta)
-	
-	faceMouse()
-	moveCameraFollow()
-	
-	if Input.is_action_pressed("fire") and playerGun.canFire():
-		playerGun.fire(get_global_mouse_position())
-		animationPlayer.play("Fire")
-
-
-func move_state(delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = input_vector.normalized()
-	
+	var input_vector = get_input_vector()
 	if input_vector != Vector2.ZERO:
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move()
+	faceMouse()
+	update_animation(input_vector)
+	moveCameraFollow()
 	
+	if playerGun != null and Input.is_action_pressed("fire") and playerGun.canFire():
+		playerGun.fire(get_global_mouse_position())
+		torsoAnimationPlayer.play("HoldingGunFiring")
+		headAnimationPlayer.play("FiringHead")
+	
+func get_input_vector():
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	return input_vector
+
+func update_animation(input_vector):
+	if input_vector != Vector2.ZERO:
+		var animation_input_vector = Vector2(input_vector.x, -input_vector.y)
+		feetSprite.global_rotation = feetSpriteDefaultRotation + input_vector.angle()
+		feetAnimationPlayer.play("WalkingFeet")
+		if playerGun == null:
+			torsoAnimationPlayer.play("WalkingTorso")
+	else:
+		feetAnimationPlayer.play("IdleFeet")
+		if playerGun == null:
+			torsoAnimationPlayer.play("IdleTorso")
+
+
 func move():
 	velocity = move_and_slide(velocity)
 
@@ -64,3 +70,10 @@ func moveCameraFollow():
 	var mousePos = get_global_mouse_position()
 	var midpoint = (mousePos - global_position) / CAMERA_POSITION_OFFSET_DENOMINATOR
 	cameraFollow.global_position = global_position + midpoint
+
+func spawnPlayerGun():
+	playerGun = PlayerGun.instance()
+	add_child(playerGun)
+	playerGun.global_position = global_position
+	playerGun.rotation = rotation
+	torsoAnimationPlayer.play("HoldingGunIdle")
