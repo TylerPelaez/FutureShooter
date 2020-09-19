1cc340175cd558de98c0d633154c963e28b2acb5
TylerPelaez <tyler.pelaez.11@gmail.com>
2020-09-19 17:15:08 -0400

    Gun can be picked up and dropped

--- Enemies/StationaryEnemy.gd
+++ Enemies/StationaryEnemy.gd
@@ -1,23 +1,37 @@
 extends "res://Enemies/Enemy.gd"
 
-const EnemyGun = preload("res://Guns/Gun.tscn")
+const EnemyGun = preload("res://Guns/Pistol.tscn")
+# This is always true?
+const SPAWN_WITH_GUN = true
+export var ROTATION_SPEED := 100.0
 
 var enemyGun = null
+
+onready var tween = $Tween
 onready var playerDetectionZone = $PlayerDetectionZone
+onready var animationPlayer = $AnimationPlayer
 
 func _ready():
-	spawnEnemyGun()
+	if SPAWN_WITH_GUN:
+		spawnEnemyGun()
 
 func _physics_process(delta):
 	if playerDetectionZone.isAwareOfPlayer():
 		var playerPosition = playerDetectionZone.getPlayerPosition()
-		look_at(playerPosition)
+		turn(playerPosition)
 		if enemyGun != null and enemyGun.canFire():
 			enemyGun.fire(playerPosition)
+			animationPlayer.play("Firing")
+	
+func turn(targetPosition):
+	var initial_transform = Vector2(global_position.x, global_position.y)
+	var duration = abs(targetPosition.x - initial_transform.x) / ROTATION_SPEED
+	
+	# interpolate
+	tween.interpolate_method(self, 'look_at', initial_transform, targetPosition,  duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
 	
 func spawnEnemyGun():
 	enemyGun = EnemyGun.instance()
-	add_child(enemyGun)
-	enemyGun.global_position = global_position
-	enemyGun.rotation = rotation
-	enemyGun.setMask(3)
+	enemyGun.pickup(self, 3, false)
+	enemyGun.ammoCount
--- Guns/Gun.gd
+++ Guns/Gun.gd
@@ -1,21 +1,26 @@
-extends Sprite
+extends KinematicBody2D
 
 const DefaultProjectile = preload("res://Guns/Projectile.tscn")
 
-export var BULLET_SPEED := 500
+export var BULLET_SPEED := 1000
 export var MAX_AMMO := 2
-onready var ammoCount = MAX_AMMO
 
+onready var ammoCount = MAX_AMMO 
 onready var fireTimer = $FireTimer
 onready var muzzle = $Muzzle
+onready var collider = $Collider
+onready var pickupRange = $PickupRange/Collider
 
+var usesAmmo = true
 var mask = 0
+var dropped = true
 
 func canFire():
-	return ammoCount > 0 and fireTimer.time_left <= 0
+	return (!usesAmmo or ammoCount > 0) and fireTimer.time_left <= 0
 
 func fire(targetPosition):
-	ammoCount = max(ammoCount -1, 0)
+	if usesAmmo:
+		ammoCount = max(ammoCount -1, 0)
 	instantiateProjectile(targetPosition)
 	fireTimer.start()
 
@@ -27,5 +32,24 @@ func instantiateProjectile(targetPosition):
 	projectile.rotation = projectile.velocity.angle()
 	projectile.setMask(mask)
 
-func setMask(index):
-	mask = index
+func pickup(newParent, newMask, useAmmo):
+	if get_parent() != null:
+		get_parent().remove_child(self)
+	newParent.add_child(self)
+	global_position = newParent.global_position
+	global_rotation = newParent.global_rotation
+	mask = newMask
+	collider.disabled = true
+	dropped = false
+	usesAmmo = useAmmo
+	pickupRange.disabled = true
+
+func drop(world, newPosition, newRotation):
+	if get_parent() != null:
+		get_parent().remove_child(self)
+	world.add_child(self)
+	global_position = newPosition
+	global_rotation = newRotation
+	collider.disabled = false
+	dropped = true
+	pickupRange.disabled = false
