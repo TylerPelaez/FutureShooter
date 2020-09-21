c1b52b9540ab816854714aca7c87ad41b92dae9a
unknown <chuangmatthew@yahoo.com>
2020-09-20 20:09:11 -0400

    add flashing logo on title screen

--- Menu/TitleMenu.gd
+++ Menu/TitleMenu.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+onready var logoAnimation = $LogoAnimationPlayer
+
 func _ready():
 	VisualServer.set_default_clear_color(Color("#238531"))
-
+	logoAnimation.play("LogoFlash")
 
 func _on_Button_pressed():
 	get_tree().change_scene(Utils.get_scene(1))
