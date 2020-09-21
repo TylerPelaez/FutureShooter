ea6817439f2d4fc25849fb8ce6a689957c28b4eb
unknown <chuangmatthew@yahoo.com>
2020-09-20 20:52:39 -0400

    Create tutorial menu

--- Menu/TitleMenu.gd
+++ Menu/TitleMenu.gd
@@ -8,3 +8,7 @@ func _ready():
 
 func _on_Button_pressed():
 	get_tree().change_scene(Utils.get_scene(1))
+
+
+func _on_Instructions_pressed():
+	get_tree().change_scene("res://Menu/TutorialMenu.tscn")
